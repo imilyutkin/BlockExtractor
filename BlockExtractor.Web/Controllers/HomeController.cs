0bda01f0a55201670686e9861ba27b67897b7b61
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2015-05-16 12:30:11 +0300

    added simples controlllers

---
+++ BlockExtractor.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace BlockExtractor.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        //
+        // POST: /Home/
+        [HttpPost]
+        public ActionResult Index(SiteUrlViewModel siteUrlModel)
+        {
+            return RedirectToAction("Extract", "Home", siteUrlModel);
+        }
+
+        [HttpGet]
+        public ActionResult Extract(SiteUrlViewModel siteUrlModel)
+        {
+            return View(siteUrlModel);
+        }
+
+    }
+
+    public class SiteUrlViewModel
+    {
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+}
