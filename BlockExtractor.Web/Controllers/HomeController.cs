029f63d9dafa288d841ef5870bba0c368b347ee2
Ivan Milyutkin <ivan.milyutkin@gmail.com>
2015-05-16 16:08:03 +0300

    added simple highlighter

--- BlockExtractor.Web/Controllers/HomeController.cs
+++ BlockExtractor.Web/Controllers/HomeController.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
+using HtmlAgilityPack;
 
 namespace BlockExtractor.Web.Controllers
 {
@@ -24,7 +28,45 @@ namespace BlockExtractor.Web.Controllers
         [HttpGet]
         public ActionResult Extract(SiteUrlViewModel siteUrlModel)
         {
-            return View(siteUrlModel);
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(siteUrlModel.Url);
+            var textNodes =
+                doc.DocumentNode.Descendants("section")
+                    .Where(d => d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("b-main-page-news-2__main-news"));
+//            foreach (var htmlNode in textNodes)
+//            {
+//                WriteText(doc, htmlNode, "lol");
+//            }
+
+            foreach (var node in textNodes)
+            {
+                if (node.Attributes.Contains("class"))
+                {
+                    var classes = node.Attributes["class"].Value;
+                    classes += " green-bg";
+                    node.Attributes["class"].Value = classes;
+                }
+                else
+                {
+                    node.Attributes.Add("class", "green-bg");
+                }
+            }
+
+    
+            var html = doc.DocumentNode.OuterHtml;
+            return View(new HtmlString(html));
+        }
+
+        private void WriteText(HtmlDocument htmlDocument, HtmlNode node, string text)
+        {
+            if (node.ChildNodes.Count > 0)
+            {
+                node.ReplaceChild(htmlDocument.CreateTextNode(text), node.ChildNodes.First());
+            }
+            else
+            {
+                node.AppendChild(htmlDocument.CreateTextNode(text));
+            }
         }
 
     }
